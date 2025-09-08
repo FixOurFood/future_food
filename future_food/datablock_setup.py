0db009cc188088a6793f158de77b1514b0ca3c19
jucordero <obejaamarilla@hotmail.com>
2025-09-08 18:21:50 +0100

    changed location of landa data array

--- future_food/datablock_setup.py
+++ future_food/datablock_setup.py
@@ -177,7 +177,8 @@ def datablock_setup(
     # Get AES key & IV from secrets
     AES_KEY = base64.b64decode(AES_KEY)
     AES_IV = base64.b64decode(AES_IV)
-    with open("UKCEH_LC_target_percentage.bin", "rb") as f:
+    import importlib.resources
+    with importlib.resources.files("future_food.data").joinpath("UKCEH_LC_target_percentage.bin").open("rb") as f:
         encrypted_data = f.read()
 
     # Decrypt the dataset
